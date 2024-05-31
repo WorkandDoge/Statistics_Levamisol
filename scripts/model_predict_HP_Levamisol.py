b2816f2dddfa28814dbc97ebe2a6e1f5f66ce1aa
WorkandDoge <152023820+WorkandDoge@users.noreply.github.com>
2024-05-31 19:59:47 +0200

    adjusted n_components of PCA

--- scripts/model_predict_HP_Levamisol.py
+++ scripts/model_predict_HP_Levamisol.py
@@ -19,7 +19,7 @@ def load_data(file_path):
 
 def perform_pca(data):
     # Perform PCA
-    pca = PCA(n_components=0.93)
+    pca = PCA(n_components=0.99)
     principal_components = pca.fit_transform(data)
     return principal_components
 
@@ -35,11 +35,11 @@ def evaluate_model(model, X_test, y_test):
     print(f"Accuracy: {accuracy}")
 
 def main():
-    file_path = '/Users/carinaobermuller/Documents/Statistics_Levamisol/output_files/fuseddata_HP_COC_Total.csv' # change to the path of the merged data file with the variable to predict
+    file_path = '/Users/carinaobermuller/Documents/Statistics_Levamisol/output_files/fuseddata_HP_Levamisol.csv' # change to the path of the merged data file with the variable to predict
     data = load_data(file_path)
     principal_components = perform_pca(data.iloc[:, 1:])
     X = principal_components
-    y = data['HP_COC_Total'] # change according to the column name in the data
+    y = data['HP_Levamisol'] # change according to the column name in the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
     model = train_model(X_train, y_train)
     evaluate_model(model, X_test, y_test)
